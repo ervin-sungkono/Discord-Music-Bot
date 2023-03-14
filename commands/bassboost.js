1e5594776b399060858ff084a4a1c18aeebd6f15
ervin-sungkono <ervinsung@gmail.com>
2023-03-14 22:47:52 +0700

    small fix in bassboost command

--- commands/bassboost.js
+++ commands/bassboost.js
@@ -9,12 +9,14 @@ module.exports = {
         const queue = player.nodes.get(interaction.guildId)
         if (!queue) return client.error.DEFAULT_ERROR(interaction)
 
-        const isEnabled = await queue.filters.ffmpeg.toggle(['bassboost', 'normalizer2']);
+        const success = await queue.filters.ffmpeg.toggle(['bassboost', 'normalizer2']);
 
         setTimeout(() => {
             return interaction.followUp({
-                content: `Bassboost ${isEnabled ? 'Enabled' : 'Disabled'}! ✅` 
-            });
-        }, queue.node.bufferingTimeout);
+                content: success ? 
+                `Bassboost ${queue.filters.ffmpeg.isEnabled('bassboost') ? 'Enabled' : 'Disabled'}! ✅` :
+                `Something went wrong ${interaction.member}... try again ? ❌`
+            })
+        }, queue.node.bufferingTimeout)
     }
 }
